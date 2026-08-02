91312ec866f4dfa501f8073c2a0cda5a40a4f55d
1brawlstars1420-ctrl <1brawlstars1420@gmail.com>
2026-08-02 15:15:23 -0500

    Update Main.java

--- src/main/java/com/ak4clan/plugin/Main.java
+++ src/main/java/com/ak4clan/plugin/Main.java
@@ -7,10 +7,15 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("¡AK4CLAN ha sido activado correctamente en Purpur!");
+        
+        // Registrar el comando /clan
+        if (getCommand("clan") != null) {
+            getCommand("clan").setExecutor(new ClanCommand());
+        }
     }
 
     @Override
     public void onDisable() {
         getLogger().info("AK4CLAN se ha desactivado.");
     }
-}
+                }
