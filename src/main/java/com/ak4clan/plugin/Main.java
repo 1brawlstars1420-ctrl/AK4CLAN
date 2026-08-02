a27a380c322def6be5966fdafd8c712a485c9b96
1brawlstars1420-ctrl <1brawlstars1420@gmail.com>
2026-08-02 15:51:29 -0500

    Update Main.java

--- src/main/java/com/ak4clan/plugin/Main.java
+++ src/main/java/com/ak4clan/plugin/Main.java
@@ -1,21 +1,27 @@
-package com.ak4clan.plugin;
+package com.ak4clan;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
+    
+    private static Main instance;
 
     @Override
     public void onEnable() {
-        getLogger().info("¡AK4CLAN ha sido activado correctamente en Purpur!");
+        instance = this;
+        getLogger().info("AK4CLAN ha sido activado correctamente con interfaz interactiva.");
         
-        // Registrar el comando /clan
-        if (getCommand("clan") != null) {
-            getCommand("clan").setExecutor(new ClanCommand());
-        }
+        // Registrar comandos y eventos
+        getCommand("clan").setExecutor(new ClanCommand());
+        getServer().getPluginManager().registerEvents(new ClanListener(), this);
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("AK4CLAN se ha desactivado.");
+        getLogger().info("AK4CLAN desactivado.");
     }
-                }
+
+    public static Main getInstance() {
+        return instance;
+    }
+}
